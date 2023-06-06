a8051fa76ad6193d2a5efb84a988b6ba2f89cee6
Max Programming <ui.sabuwala@gmail.com>
2023-06-07 02:48:01 +0530

    Playgrounds Section Done

--- src/app/profile/edit/portfolio/page.tsx
+++ src/app/profile/edit/portfolio/page.tsx
@@ -1,12 +1,9 @@
+import { Playgrounds } from '@/components/ProfileComponents/Portfolio';
+
 export default function PortfolioEditPage() {
   return (
     <div>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque,
-        molestias voluptate quisquam libero cum nulla alias delectus beatae!
-        Optio consectetur enim voluptatum ducimus autem laudantium harum amet
-        tempore blanditiis ipsam.
-      </p>
+      <Playgrounds isEditing />
     </div>
   );
 }
--- src/components/ProfileComponents/Portfolio/Playgrounds.tsx
+++ src/components/ProfileComponents/Portfolio/Playgrounds.tsx
@@ -1,96 +1,21 @@
-/* eslint-disable @next/next/no-img-element */
 import Button from '@/components/ui/Button';
-import { Card } from '@/components/ui/Card';
-import { Html5Logo, JSLogo } from '@/icons';
-import { ReactNode } from 'react';
+import { PlaygroundCards } from './PlaygroundCard';
 
-export function Playgrounds() {
+export function Playgrounds(props: { isEditing?: boolean }) {
   return (
     <div>
-      <div className='mb-2 flex justify-between'>
+      <div className='mb-2 flex items-center justify-between'>
         <h3 className='mb-5 text-3xl font-bold'>Playgrounds</h3>
-        <Button className='font-semibold text-primary hover:bg-border'>
-          Create new playground
+        <Button
+          className={`font-semibold hover:bg-border ${
+            props.isEditing ? 'text-secondary' : 'text-primary'
+          }`}
+        >
+          {props.isEditing ? 'See all' : 'Create new playground'}
         </Button>
       </div>
 
-      <div className='grid grid-cols-2 gap-5'>
-        {plays.map((play, idx) => (
-          <PlaygroundCard key={idx} playground={play} />
-        ))}
-      </div>
+      <PlaygroundCards />
     </div>
   );
 }
-
-function PlaygroundCard(props: PlaygroundCardProps) {
-  return (
-    <Card>
-      <div className='flex gap-4'>
-        {props.playground.logo}
-        <div className='flex flex-col gap-2'>
-          <h3 className='text-2xl font-bold'>{props.playground.title}</h3>
-          <p className='text-secondary'>
-            {props.playground.tech} • {props.playground.timeago} ago
-          </p>
-          <div className='flex flex-col items-start md:flex-row md:items-center md:space-x-3'>
-            <div className='flex -space-x-2'>
-              <img
-                src='/profile2.png'
-                alt=''
-                className='h-10 w-10 rounded-full'
-              />
-              <img
-                src='/profile.jpeg'
-                alt=''
-                className='h-10 w-10 rounded-full'
-              />
-            </div>
-            <p className='text-secondary'>
-              Shared with <span className='font-bold'>Adam,</span>{' '}
-              <span className='font-bold'>Anna..</span> +7 more
-            </p>
-          </div>
-        </div>
-      </div>
-    </Card>
-  );
-}
-
-interface PlaygroundType {
-  title: string;
-  tech: string;
-  timeago: string;
-  logo: ReactNode;
-}
-
-type PlaygroundCardProps = {
-  playground: PlaygroundType;
-};
-
-const plays: PlaygroundType[] = [
-  {
-    title: 'HTML Playground',
-    tech: 'HTML/CSS',
-    timeago: '1 day',
-    logo: <Html5Logo width='40' height='40' />,
-  },
-  {
-    title: 'JS Playground',
-    tech: 'JavaScript',
-    timeago: '1 min',
-    logo: <JSLogo width='40' height='40' />,
-  },
-  {
-    title: 'JS Playground',
-    tech: 'JavaScript',
-    timeago: '1 min',
-    logo: <JSLogo width='40' height='40' />,
-  },
-  {
-    title: 'HTML Playground',
-    tech: 'HTML/CSS',
-    timeago: '1 day',
-    logo: <Html5Logo width='40' height='40' />,
-  },
-];

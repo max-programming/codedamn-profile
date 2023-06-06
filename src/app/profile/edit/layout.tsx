edaeb00c2b0e4130cc0c4d89f268a7e3534824ac
Max Programming <ui.sabuwala@gmail.com>
2023-06-07 02:14:34 +0530

    Add footer with save and cancel buttons

--- src/app/profile/edit/layout.tsx
+++ src/app/profile/edit/layout.tsx
@@ -1,11 +1,20 @@
+import Button from '@/components/ui/Button';
 import { Sidebar } from '@/components/ui/Sidebar';
 import { PropsWithChildren } from 'react';
 
 export default function EditLayout({ children }: PropsWithChildren) {
   return (
-    <div className='flex h-full gap-16'>
-      <Sidebar />
-      <main className='flex-1'>{children}</main>
+    <div className='h-full'>
+      <div className='flex gap-16'>
+        <Sidebar />
+        <main className='flex-1'>{children}</main>
+      </div>
+      <footer className='mt-5 flex justify-end gap-5'>
+        <Button className='bg-border font-semibold'>Cancel</Button>
+        <Button className='bg-primary font-semibold text-white'>
+          Save changes
+        </Button>
+      </footer>
     </div>
   );
 }

5b3084f908d243f5ecb2c7bf9ab0b023913a783b
Max Programming <ui.sabuwala@gmail.com>
2023-06-06 17:37:57 +0530

    Resume About and Work Experience

--- src/app/profile/resume/page.tsx
+++ src/app/profile/resume/page.tsx
@@ -1,10 +1,13 @@
 import { ProfileCard, ProfileTabs } from '@/components/ProfileComponents';
+import { About, Work } from '@/components/ProfileComponents/Resume';
 
 export default function Home() {
   return (
     <div className='flex flex-col gap-10'>
       <ProfileCard />
       <ProfileTabs />
+      <About />
+      <Work />
     </div>
   );
 }
